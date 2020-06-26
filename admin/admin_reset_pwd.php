fbe17aee4fa4ca365810fe3bcc6f3a246d5fbc67
martin <martdevelopers254@gmail.com>
2020-06-26 22:32:36 +0300

    Implemented SuperUser Auth Frontend scaffoldings

---
+++ admin/admin_reset_pwd.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+    <!--Head-->
+    <?php require_once("partials/head.php");?>
+    <!-- ./ Head -->
+    <body class="form no-image-content">
+        <div class="form-container outer">
+            <div class="form-form">
+                <div class="form-form-wrap">
+                    <div class="form-container">
+                        <div class="form-content">
+                            <h1 class="">Password Recovery</h1>
+                            <p class="signup-link recovery">Enter your email and instructions will sent to you!</p>
+                            <form class="text-left" method="post">
+                                <div class="form">
+                                    <div id="email-field" class="field-wrapper input">
+                                        <div class="d-flex justify-content-between">
+                                            <label for="email">EMAIL</label>
+                                        </div>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-at-sign"><circle cx="12" cy="12" r="4"></circle><path d="M16 8v5a3 3 0 0 0 6 0v-1a10 10 0 1 0-3.92 7.94"></path></svg>
+                                        <input id="email" name="email" type="text" class="form-control" value="" placeholder="Email">
+                                    </div>
+
+                                    <div class="d-sm-flex justify-content-between">
+
+                                        <div class="field-wrapper">
+                                            <button type="submit" class="btn btn-primary" value="">Reset</button>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </form>
+
+                        </div>                    
+                    </div>
+                </div>
+            </div>
+        </div>   
+    <!-- BEGIN GLOBAL MANDATORY SCRIPTS -->
+    <script src="assets/js/libs/jquery-3.1.1.min.js"></script>
+    <script src="bootstrap/js/popper.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    
+    <!-- END GLOBAL MANDATORY SCRIPTS -->
+    <script src="assets/js/authentication/form-2.js"></script>
+
+</body>
+</html>
---
+++ admin/partials/head.php
@@ -0,0 +1,26 @@
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
+    <title>liteERP-Admin panel</title>
+    <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico"/>
+    <link href="assets/css/loader.css" rel="stylesheet" type="text/css" />
+    <script src="assets/js/loader.js"></script>
+    <!-- BEGIN GLOBAL MANDATORY STYLES -->
+    <link href="https://fonts.googleapis.com/css?family=Quicksand:400,500,600,700&amp;display=swap" rel="stylesheet">
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/plugins.css" rel="stylesheet" type="text/css" />
+    <!-- END GLOBAL MANDATORY STYLES -->
+
+    <!-- Dashboard LEVEL PLUGINS/CUSTOM STYLES -->
+    <link href="plugins/apex/apexcharts.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/dashboard/dash_2.css" rel="stylesheet" type="text/css" />
+    <!--  ./ Dashboard LEVEL PLUGINS/CUSTOM STYLES -->
+
+    <!--Auth CSS STYLES -->
+    <link href="assets/css/authentication/form-2.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" type="text/css" href="assets/css/forms/theme-checkbox-radio.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/forms/switches.css">
+    <!-- ./ Auth CSS Styles -->
+
+</head>
