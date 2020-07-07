1a5e724b1b78dd5b25a36ca2beb8a50ddd035799
martin <martdevelopers254@gmail.com>
2020-07-07 18:29:41 +0300

    Implemented Lock Screen

--- admin/admin_lockscreen.php
+++ admin/admin_lockscreen.php
@@ -1,10 +1,24 @@
 <?php
     session_start();
     require_once('config/config.php');
-
+    
     //handle unclock
     if(isset($_POST['unlock']))
     {
+        //Implement Error handling
+        $error = 0;
+
+        //prevent posting blank value for password
+        if (isset($_POST['admin_password']) && !empty($_POST['admin_password']))
+        {
+            $admin_password = mysqli_real_escape_string($conn,trim(sha1(md5($_POST['admin_password']))));
+        }
+        else
+        {
+            $error = 1;
+            $err="Password Cannot Be Empty";
+        }
+
         $admin_email = $_POST['admin_email'];
         $admin_password = sha1(md5($_POST['admin_password']));//double encrypt to increase security
         $stmt=$conn->prepare("SELECT admin_email, admin_password  FROM liteERP_admin  WHERE admin_email =? AND admin_password =?");
@@ -33,21 +47,23 @@
         <?php
             $admin_id = $_SESSION['admin_id'];
             $ret="SELECT * FROM `liteERP_admin` WHERE admin_id =? "; 
-            $stmt= $mysqli->prepare($ret) ;
+            $stmt= $conn->prepare($ret) ;
             $stmt->bind_param('i', $admin_id);
             $stmt->execute();
             $res=$stmt->get_result();
             $cnt=1;
-            while($row=$res->fetch_object())
+            while($superAdmin = $res->fetch_object())
             {
                 //Show default image if logged in user has no profile pic
-                if($row->icon == '')
+                if($superAdmin->admin_dpic == '')
                 {
-                    $img = "<img src='assets/img/police_officers/profile.png' class='usr-profile' alt='avatar'>";
+                    $img = "<img src='assets/img/boy.png' class='usr-profile' alt='avatar'>";
+
+                    
                 }
                 else
                 {
-                    $img = "<img src='assets/img/police_officers/$row->icon' class='usr-profile' alt='avatar'>";
+                    $img = "<img src='assets/img/$superAdmin->admin_dpic' class='usr-profile' alt='avatar'>";
                 }
         ?>
             <div class="form-container">
@@ -57,42 +73,30 @@
                             <div class="form-content">
 
                                 <div class="d-flex user-meta">
-                                    <img src="assets/img/profile-7.jpg" class="usr-profile" alt="avatar">
+                                    <?php echo $img;?>
                                     <div class="">
-                                        <p class="">Shaun Park</p>
+                                        <p class=""><?php echo $superAdmin->admin_name;?></p>
                                     </div>
                                 </div>
 
-                                <form method="post" class="text-left">
+                                <form method="post" method="post" class="text-left">
                                     <div class="form">
+                                        <div id="" class="field-wrapper input mb-2" style="display:none">
+                                            <input id="email" name="admin_email"  type="email" value="<?php echo $superAdmin->admin_email;?>" class="form-control" placeholder="Password">
+                                        </div>
 
                                         <div id="password-field" class="field-wrapper input mb-2">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-lock"><rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect><path d="M7 11V7a5 5 0 0 1 10 0v4"></path></svg>
-                                            <input id="password" name="password" type="password" class="form-control" placeholder="Password">
+                                            <input id="password" name="admin_password" type="password" class="form-control" placeholder="Password">
                                         </div>
                                         <div class="d-sm-flex justify-content-between">
-                                            <div class="field-wrapper toggle-pass">
-                                                <p class="d-inline-block">Show Password</p>
-                                                <label class="switch s-primary">
-                                                    <input type="checkbox" id="toggle-password" class="d-none">
-                                                    <span class="slider round"></span>
-                                                </label>
-                                            </div>
                                             <div class="field-wrapper">
-                                                <button type="submit" class="btn btn-primary" value="">Unlock</button>
+                                                <button type="submit" name="unlock" class="btn btn-primary" value="">Unlock</button>
                                             </div>
-                                            
                                         </div>
-
                                     </div>
                                 </form>                        
-                                <p class="terms-conditions">© 2020 - <?php echo date("Y");?> All Rights Reserved. Powered By <a target ="_blank" href="https://devlan.martdev.info">Devlan</a>. </p>
                             </div>                    
                         </div>
-                    </div>
-                </div>
-                <div class="form-image">
-                    <div class="l-image">
                     </div>
                 </div>
             </div>
