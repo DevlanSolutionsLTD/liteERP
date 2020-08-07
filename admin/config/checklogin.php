b3b4daaa2891b57614a3148414d4889f805eed74
martin <martdevelopers254@gmail.com>
2020-08-07 18:52:32 +0300

    Refactored Page names

--- admin/config/checklogin.php
+++ admin/config/checklogin.php
@@ -5,7 +5,7 @@
 			{
 				$host = $_SERVER['HTTP_HOST'];
 				$uri  = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
-				$extra="admin_login.php";
+				$extra="index.php";
 				$_SESSION["login_user_email"]="";
 				header("Location: http://$host$uri/$extra");
 			}
